0069b7746d17f47f5e473c40462e3322ed7e0ab0
wenbin <bin.wen@connext.com.cn>
2020-04-20 18:14:10 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Layout>
+      <router-view></router-view>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from './components/layout/Layout'
+export default {
+  name: 'App',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+html,body,#app{
+  height: 100%;
+}
+</style>
---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-container style="height:100%">
+        <el-aside height='100%' width="100px" style="background-color: rgb(238, 241, 246);height:100%"></el-aside>
+        <el-container>
+            <el-header >12</el-header>
+            <el-main>12</el-main>
+        </el-container>
+        
+    </el-container>
+</template>
+<script>
+export default {
+  name: 'Layout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang = "sass" scoped>
+
+</style>
